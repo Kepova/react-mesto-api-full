80a8a20e6506fea1799d00dbda2ed71260e567b4
Kepova Valentina <v.kepova@gmail.com>
2022-08-19 00:24:09 +0500

    настроить cors

--- frontend/src/utils/mestoAuth.js
+++ frontend/src/utils/mestoAuth.js
@@ -30,7 +30,7 @@ export const login = ({email, password}) => {
             "access-control-request-headers": "https://mesto.kepova.nomoredomains.sbs",
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ email, password })
+        body: { email, password }
     })
         .then(CheckResponse)
 }
