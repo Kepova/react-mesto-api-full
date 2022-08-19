c5a04f245c856b6c6488e37ef933b632eaf2f437
Kepova Valentina <v.kepova@gmail.com>
2022-08-19 14:26:27 +0500

    заголовки в ответ при авторизации пользователя

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -83,7 +83,6 @@ const login = (req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
-    res.end();
   }
   User.findUserByCredentials({ email, password })
     .then((user) => {
