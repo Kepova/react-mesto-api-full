583731e3d0588cbaefa406d7a893134677bd6e4f
Kepova Valentina <v.kepova@gmail.com>
2022-08-19 13:32:59 +0500

    настроить cors2

--- backend/app.js
+++ backend/app.js
@@ -36,13 +36,12 @@ app.use((req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
   }
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-
+  res.end();
   next();
 });
 
