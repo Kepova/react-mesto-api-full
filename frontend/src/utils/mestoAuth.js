2f6daaa4a5732686b723321d788945deaea1a243
Kepova Valentina <v.kepova@gmail.com>
2022-08-19 16:29:34 +0500

    настроить загрузку данных на страницу

--- frontend/src/utils/mestoAuth.js
+++ frontend/src/utils/mestoAuth.js
@@ -1,17 +1,16 @@
 export const BASE_URL = 'https://api.mesto.kepova.nomoredomains.sbs';
 
 const CheckResponse = (res) => {
-
     if (res.ok) {
         return res;
     }
-    return res.json()
+    return res
         .then((data) => {
             throw new Error(data.message);
         })
 }
 
-export const register = ({email, password}) => {
+export const register = ({ email, password }) => {
     return fetch(`${BASE_URL}/signup`, {
         method: 'POST',
         headers: {
@@ -23,12 +22,11 @@ export const register = ({email, password}) => {
         .then(CheckResponse)
 }
 
-export const login = ({email, password}) => {
+export const login = ({ email, password }) => {
     return fetch(`${BASE_URL}/signin`, {
         method: 'POST',
         credentials: 'include',
         headers: {
-            "Access-Control-Allow-Origin": "https://mesto.kepova.nomoredomains.sbs",
             "access-control-request-headers": "https://mesto.kepova.nomoredomains.sbs",
             "Content-Type": "application/json"
         },
