f78c741a92b3d65e350ed20c3d86d70020804e34
Kepova Valentina <v.kepova@gmail.com>
2022-08-21 19:25:24 +0500

    исправить регулярное выражение

--- backend/constants/constants.js
+++ backend/constants/constants.js
@@ -1,4 +1,4 @@
-const regexUrl = /^(https?:\/\/)?(www\.)?([\w\d\-]+[.$])+([a-z]{2,10})\/?([^:\s]([a-z\d\W_-]{2,})*([#]$)?)*/;
+const regexUrl = /^(https?:\/\/)?(www\.)?([\w\d-]+[.$])+([a-z]{2,10})\/?([^:\s]([a-z\d\W_-]{2,})*([#]$)?)*/;
 
 module.exports = {
   regexUrl,
