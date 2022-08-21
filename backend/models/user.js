7391fc37dab50158c2b57fc9bf320a00614961bc
Kepova Valentina <v.kepova@gmail.com>
2022-08-21 19:37:58 +0500

    исправить регулярное выражение

--- backend/models/user.js
+++ backend/models/user.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator(v) {
-        return regexUrl.test(v);
+        return /^(https?:\/\/)?(www\.)?([\w\d-]+[.$])+([a-z]{2,10})\/?([^:\s]([a-z\d\W_-]{2,})*([#]$)?)*/.test(v);
       },
     },
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
